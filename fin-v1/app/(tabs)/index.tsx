016aae2a0b39b6546ed368c05f5c21c7b852c3d0
Srivathsan <treewhacksun@localhost.localdomain>
2025-12-09 15:25:24 -0500

    feat: setup adb and usb debugging for my pixel 8 w/ development build

--- fin-v1/app/(tabs)/index.tsx
+++ fin-v1/app/(tabs)/index.tsx
@@ -33,7 +33,7 @@ export default function HomeScreen() {
               web: 'F12',
             })}
           </ThemedText>{' '}
-          to open developer tools.
+          to open developer tools, but on wire/USB debugging 😏.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
